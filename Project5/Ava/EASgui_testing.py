cb77129aca9a75ba7bc23c6be137f18c107fe51f
avalamont <avalamont@gmail.com>
2025-04-03 12:34:04 -0400

    Update EASgui_testing.py

--- Project5/Ava/EASgui_testing.py
+++ Project5/Ava/EASgui_testing.py
@@ -1,20 +1,37 @@
-############# testing the gui - adding serial connections next #######
+
+############# testing the gui - serial connections next #######
+
 import tkinter as tk
+from enum import nonmember
 from tkinter import filedialog
+import threading
+import serial
+import PIL
 from PIL import ImageGrab
 
+#from GUI_testing import ser
+
+try:
+    ser = serial.Serial('COM19', 9600, timeout=1)
+    print(f"Connected to: {ser.name}")
+except serial.SerialException as e:
+    print(f"Error: Could not open serial port: {e}")
+    ser = None
+
 class CanvasTest:
-    def __init__(self,root):
+############ initialize GUI ##############################################
+    def __init__(self, root):
         self.root = root
         self.window = root
         self.left_frame = tk.Frame()
         self.left_frame.pack(side=tk.LEFT, padx=20, pady=20)
 
+        self.game_active = True
+
         self.w = 512
         self.h = 512
         self.pencolor = "black"
         self.penwidth = 1
-
 
         self.canvas = tk.Canvas(self.left_frame, width=self.w, height=self.h, bg="white")
         self.canvas.pack()
@@ -24,13 +41,16 @@ class CanvasTest:
         self.right_frame = tk.Frame()
         self.right_frame.pack(side=tk.RIGHT, padx=80, pady=80)
 
-        self.canvasz_label = tk.Label(self.right_frame, text=f"sketching area: {self.w}x{self.h}" , font=("fixedsys", 14), fg="black")
-        self. canvasz_label.pack()
+        self.canvasz_label = tk.Label(self.right_frame, text=f"sketching area: {self.w}x{self.h}",
+                                      font=("fixedsys", 14), fg="black")
+        self.canvasz_label.pack()
         # pen color being displayed
-        self.pencolor_label = tk.Label(self.right_frame, text=f"pen color: {self.pencolor}",font=("fixedsys", 14), fg="black")
+        self.pencolor_label = tk.Label(self.right_frame, text=f"pen color: {self.pencolor}", font=("fixedsys", 14),
+                                       fg="black")
         self.pencolor_label.pack()
         # pen width
-        self.penwidth_label = tk.Label(self.right_frame, text=f"pen width: {self.penwidth}", font=("fixedsys", 14), fg="black")
+        self.penwidth_label = tk.Label(self.right_frame, text=f"pen width: {self.penwidth}", font=("fixedsys", 14),
+                                       fg="black")
         self.penwidth_label.pack()
         # x position
         self.xpos_label = tk.Label(self.right_frame, text="x: ", font=("fixedsys", 14), fg="black")
@@ -38,23 +58,27 @@ class CanvasTest:
         # y position
         self.ypos_label = tk.Label(self.right_frame, text="y: ", font=("fixedsys", 14), fg="black")
         self.ypos_label.pack()
-        #save button
+        # save button
         self.save_button = tk.Button(self.right_frame, text="Save Canvas as PNG", command=self.save_canvas)
         self.save_button.pack(pady=10)
-
+        # used in drawing horizontal lines so width is same as a vertical line
         self.prev_x = self.x
         self.prev_y = self.y
-        
+
+        self.serial_thread = threading.Thread(target=self.read_serial_input, daemon=True)
+        self.serial_thread.start()
+
+        # binding key presses to functions
         self.canvas.bind("<KeyPress>", self.draw)
         self.canvas.bind("<KeyRelease>", self.handle_keyrel)
         self.canvas.focus_set()
-        
-        
-    def handle_keyrel(self, event):
+
+    def handle_keyrel(self, event): #handling multiple functions with keyrelease binds
         self.colors(event)
         self.pwidth(event)
         self.cwidth(event)
 
+##### function to change width of canvas between 256x256 and 512x512 ############
     def cwidth(self, event):
         if event.keysym in ("S", "s"):
             # get the drawing from canvas
@@ -93,12 +117,12 @@ class CanvasTest:
             self.ypos_label.config(text=f"y: {self.y}")
             self.canvas.create_line(self.x, self.y, self.x, self.y + 1, width=self.penwidth, fill=self.pencolor)
 
-    ########### changing the color of the pen ############################################
+########### changing the color of the pen ############################################
     def colors(self, event):
 
-        key_to_color = {"R": "red", "G": "green","B": "blue","O": "black",
-                        "W": "white","Y": "yellow"
-        }
+        key_to_color = {"R": "red", "G": "green", "B": "blue", "O": "black",
+                        "W": "white", "Y": "yellow"
+                        }
         color = key_to_color.get(event.keysym)
 
         if color:
@@ -109,29 +133,47 @@ class CanvasTest:
     def pwidth(self, event):
         key_to_pwidth = {"1": 1, "2": 2, "3": 3, "4": 4,
                          "5": 5, "6": 6, "7": 7
-        }
+                         }
         penwidth = key_to_pwidth.get(event.keysym)
         if penwidth:
             self.penwidth = penwidth
             self.penwidth_label.config(text=f"pen width: {self.penwidth}")
 
-  ########## testing user changing position of the pen to draw ##########################
-    def draw(self, event):
+########## testing user changing position of the pen to draw ##########################
+    def draw(self, event= None, ascii_char = None):
         self.prev_x = self.x
         self.prev_y = self.y
+        move = None
 
-        if event.keysym == "Up":
-            if self.y > 0:
-                self.y -= 1
-        elif event.keysym == "Down":
-            if self.y < self.h:
-                self.y += 1
-        elif event.keysym == "Left":
-            if self.x > 0:
-                self.x -= 1
-        elif event.keysym == "Right":
-            if self.x < self.w:
-                self.x += 1
+        if event:
+            if event.keysym == "Up":
+                move = "Up"
+            elif event.keysym == "Down":
+                move = "Down"
+            elif event.keysym == "Left":
+                move = "Left"
+            elif event.keysym == "Right":
+                move = "Right"
+
+        if ascii_char:
+            if ascii_char == "Up":
+                move = "Up"
+            elif ascii_char == "Down":
+                move = "Down"
+            elif ascii_char == "Left":
+                move = "Left"
+            elif ascii_char == "Right":
+                move = "Right"
+
+
+        if move == "Up" and self.y > 0:
+            self.y -= 1
+        elif move == "Down" and self.y < self.h:
+            self.y += 1
+        elif move == "Left" and self.x > 0:
+            self.x -= 1
+        elif move == "Right" and self.x < self.w:
+            self.x += 1
 
         # draw line
         self.canvas.create_line(self.prev_x, self.prev_y, self.x, self.y, width=self.penwidth, fill=self.pencolor)
@@ -139,7 +181,7 @@ class CanvasTest:
         self.xpos_label.config(text=f"x: {self.x}")
         self.ypos_label.config(text=f"y: {self.y}")
 
-    ########## saving the drawing as a png ##################################################
+########## saving the drawing as a png ##################################################
     def save_canvas(self):
         filename = filedialog.asksaveasfilename(
             defaultextension=".png",
@@ -154,7 +196,7 @@ class CanvasTest:
         self.root.attributes("-topmost", True)
         self.root.after_idle(lambda: self.root.attributes("-topmost", True))
 
-      # get the size of the canvas
+        # get the size of the canvas
         x = self.canvas.winfo_rootx()
         y = self.canvas.winfo_rooty()
         x1 = x + self.canvas.winfo_width()
@@ -165,7 +207,41 @@ class CanvasTest:
 
         img.save(filename)
         print(f"saved as {filename}.png")
-      
+
+
+    def read_serial_input(self):
+        UP_ARROW = b'\xE0\x48' #trying to see what values may correspond to arrow keys
+        DOWN_ARROW = b'\xE0\x50'
+        LEFT_ARROW = b'\xE0\x4B'
+        RIGHT_ARROW = b'\xE0\x74'
+
+        if  self.game_active:
+            while True:
+                if ser and ser.in_waiting > 0:
+                    data = ser.read(1)  # Read one byte
+                    if data == UP_ARROW:
+                        self.root(self.draw, ascii_char)
+                    elif data == DOWN_ARROW:
+                        self.root(self.draw, ascii_char)
+                    elif data == RIGHT_ARROW:
+                        self.root(self.draw, ascii_char)
+                    elif data == LEFT_ARROW:
+                        self.root(self.draw, ascii_char)
+
+                    else:
+                        print(f"Serial Input : {ascii_char}")
+                        ascii_char = data.decode('ascii', errors='replace').strip()
+                        self.root.after(0, self.draw, ascii_char)
+
+                    print(f"Serial Input : {ascii_char}")
+                #self.root.after(0, self.draw, ascii_char)
+                #self.root.after(0, self.pwidth, ascii_char)
+                #self.root.after(0, self.colors, ascii_char)
+                #self.root.after(0, self.cwidth, ascii_char)
+
+
+
+
 ############ calling the function ######################################################
 if __name__ == "__main__":
     root = tk.Tk()
@@ -175,7 +251,5 @@ if __name__ == "__main__":
     if ser:
         ser.close()
         print("Serial port closed.")
-
-
-#self.canvas.focus_set() 
-#self.window.mainloop()
+# self.canvas.focus_set()
+# self.window.mainloop()
